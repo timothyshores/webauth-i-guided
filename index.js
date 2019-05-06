08926e7b9fb7de2fe398b7d47cf00b729fc10422
Timothy Shores <timothymshores@gmail.com>
2019-05-06 11:56:28 -0500

    Import bcryptjs via require syntax

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
