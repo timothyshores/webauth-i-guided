0e49d811f21034c0dc594fb147a8cb2cdfcc364c
Timothy Shores <timothymshores@gmail.com>
2019-05-06 12:33:54 -0500

    Updated protected middleware logic for GET /api/users in index.js

--- index.js
+++ index.js
@@ -59,7 +59,16 @@ server.get('/api/users', protected, (req, res) => {
 function protected(req, res, next) {
     const { username, password } = req.headers;
     if (username && password) {
-        next();
+        Users.findBy({ username })
+            .first()
+            .then(user => {
+                (user && bcrypt.compareSync(password, user.password))
+                    ? next()
+                    : res.status(401).json({ message: 'Invalid Credentials' });
+            })
+            .catch(error => {
+                res.status(500).json(error);
+            });
     }
     else {
         res.status(400).json({ message: 'Invalid Credentials'})
